0662137dbcef669b5e02488820687ca54ceda40f
CParshuram <84668836+CParshuram@users.noreply.github.com>
2021-07-20 17:42:53 +0530

    my first commit reactwebsite

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+
+import "./Header.css";
+
+const Header = () => {
+    return <span onClick={() => window.scroll(0,0)} className="header">
+    🎬Diatoz ET-Hub🎥</span>
+
+        
+
+    
+    
+};
+
+export default Header;
